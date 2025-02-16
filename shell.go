3279cf2f3883fb98056ffe9672720ed69cc39e55
B <B@game>
2025-02-16 11:29:48 +0800

    *

--- shell.go
+++ shell.go
@@ -29,7 +29,7 @@ func repl() {
 	configure(shell)
 
 	shell.Println("nano REPL Client")
-	shell.SetPrompt("\033[34m➜\033[0m ")
+	shell.SetPrompt("\033[33m➜\033[0m ")
 
 	registerConnect(shell)
 	registerDisconnect(shell)
